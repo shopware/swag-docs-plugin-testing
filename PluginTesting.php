51567e5a9d774f030fb8f65a9ca6a2188375eb0b
Patrick Stahl <p.stahl@shopware.com>
2019-03-18 08:33:49 +0100

    Initial Commit

---
+++ PluginTesting.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace PluginTesting;
+
+use Shopware\Core\Framework\Plugin;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class PluginTesting extends Plugin
+{
+
+}
